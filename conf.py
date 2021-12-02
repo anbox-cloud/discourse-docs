2f4a7517e4b9790ad816bdca59f69a2c6578680f
Ruth Fuchss <ruth.fuchss@canonical.com>
2021-12-02 15:37:58 +0100

    adding next/previous buttons for easier navigation
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

--- conf.py
+++ conf.py
@@ -44,7 +44,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv', '.theme-repo']
 
-rst_epilog = """
+rst_prolog = """
+:sequential_nav: both
+
 .. include:: /reuse/substitutions.txt
 """
 
