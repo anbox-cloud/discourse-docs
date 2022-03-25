19b34106e2658ecb1b2ac631a8aa4477b8b7575c
Ruth Fuchss <ruth.fuchss@canonical.com>
2022-03-25 17:58:43 +0100

    automatically extract file names
    Update the publishing script to automatically extract the
    file name/topic ID mapping from the index page.
    
    Signed-off-by: Ruth Fuchss <ruth.fuchss@canonical.com>

--- scripts/publish.py
+++ scripts/publish.py
@@ -3,7 +3,8 @@ import sys
 import requests
 import tempfile
 
-mapping_file = "file_mapping.txt"
+index_file = "index.md"
+index_file_ID = "17029"
 discourse_prefix = "https://discourse.ubuntu.com/"
 editor = "emacs"
 discedit = "discedit"
@@ -13,30 +14,51 @@ if "EDITOR" in os.environ:
 if "DISCEDIT" in os.environ:
     discedit = os.environ['DISCEDIT']
 
+# mapping contains file names and their Discourse topic IDs
+mapping = {index_file: index_file_ID}
 
-mapping = {}
-
-with open(mapping_file,"r") as input:
+# Read the mapping from the index file
+with open(index_file,"r") as input:
+    nav = 0
 
     for line in input:
-        one = line.split(":")
-        mapping[one[0]]= one[1].strip()
+        # Only process content between "Navigation" and "Redirects"
+        if line.find("## Navigation") >= 0:
+            nav = 1
+            continue
+        elif line.find("## Redirects") >= 0:
+            nav = 0
+            continue
+        if nav:
+            row = line.split("|")
+            # If the link is a Discourse topic, use the URL as file name
+            # and extract the topic ID
+            if len(row) > 3 and row[3].find("discourse.ubuntu.com") >= 0:
+                url = row[2].strip()
+                ID = row[3].strip()[-6:-1]
+                mapping[url+".md"]= ID
 
 if len(sys.argv) != 2:
     print("You must provide one file name!")
     exit(1)
 else:
     filename = sys.argv[1]
+    # If we know the topic ID for the given file name
     if filename in mapping:
 
+        # Download the current version of the file and diff it to
+        # the file on disk
         with tempfile.NamedTemporaryFile(delete=False) as f:
             r = requests.get(discourse_prefix+"raw/"+mapping[filename])
             f.write(r.content)
+            f.write(b'\n') # Discourse file is missing a newline at the end
             oldfile = f.name
             f.close()
             os.system("diff "+oldfile+" "+filename)
             os.unlink(oldfile)
 
+        # Open the file on disk in your editor and start discedit on
+        # the corresponding topic ID
         os.system(editor+" "+filename+"&")
         os.system(discedit+" "+discourse_prefix+"t/"+mapping[filename])
     else:
